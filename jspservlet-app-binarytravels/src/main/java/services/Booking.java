69a09bc6d62f3cede8f615abe8e138e961441435
Iamneo <techadmin@iamneo.ai>
2023-08-10 09:59:20 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/services/Booking.java
+++ jspservlet-app-binarytravels/src/main/java/services/Booking.java
@@ -1,6 +1,13 @@
 package services;
 
 public class Booking{
-//	Enter code here...
+
+    public double booking(int numberOfPersons, double pricePerPerson) {
+        if (numberOfPersons <= 0 || pricePerPerson <= 0) {
+            throw new IllegalArgumentException("Number of persons and price per person must be positive.");
+        }
+        return numberOfPersons * pricePerPerson;
+    }
+    
 }
 
