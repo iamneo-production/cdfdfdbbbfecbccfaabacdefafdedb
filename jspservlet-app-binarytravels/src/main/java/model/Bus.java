1e9200c189990ac0fa9914588d149531dec201e9
Iamneo <techadmin@iamneo.ai>
2023-08-10 10:04:20 +0000

    Backup Commit

--- jspservlet-app-binarytravels/src/main/java/model/Bus.java
+++ jspservlet-app-binarytravels/src/main/java/model/Bus.java
@@ -3,6 +3,16 @@ package model;
 import java.time.LocalDate;
 
 public class Bus{
-	//Enter code here..
+	private int id;
+    private String model;
+    private int capacity;
+    private LocalDate manufacturingDate;
+
+    public Bus(int id, String model, int capacity, LocalDate manufacturingDate) {
+        this.id = id;
+        this.model = model;
+        this.capacity = capacity;
+        this.manufacturingDate = manufacturingDate;
+    }
 }
 	
--- jspservlet-app-binarytravels/src/main/java/model/Flight.java
+++ jspservlet-app-binarytravels/src/main/java/model/Flight.java
@@ -3,5 +3,10 @@ package model;
 import java.time.LocalDate;
 
 public class Flight{
-	
+	private int noOfPersons
+private String classType
+private int rates
+private LocalDate from
+private LocalDate to
+private String triptype
 }
